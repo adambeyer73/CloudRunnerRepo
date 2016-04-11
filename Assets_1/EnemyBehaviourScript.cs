56d114b612a6c58ecc473b8c548f871aada1783b
Nasar <nasarroble@gmail.com>
2016-04-11 17:12:42 -0600

    new commit
    commits

---
+++ Assets_1/EnemyBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviourScript : MonoBehaviour {
+    private Rigidbody rigidbody;
+    public float tilt;
+    Animation animation;
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        animation = GetComponent<Animation>();
+        animation.Play("Attack");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
