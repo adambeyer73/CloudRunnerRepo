7b7a3d2aabf27d525443c053b654ee73223d581e
James Corcoran <jacorcoran5.1@gmail.com>
2016-04-14 21:16:27 -0500

    Added files via upload

---
+++ Assets/basic_enemy.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class basic_enemy : MonoBehaviour {
+
+	// Use this for initialization
+    int damage = 2;
+    int health = 4;
+    /* 
+     * basic enemies wont jump but we may want 
+     * to inherit this script into more advanced enemies
+     */
+    private float localYVelocity = 0.0f;
+    double jumppwr;
+
+    public float walkSpeed = 1.0f;
+    public float wallLeft = 0.0f;
+    public float wallRight = 5.0f;
+    float walkingDirection = 1.0f;
+    Vector2 walkAmount;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	     walkAmount.x = walkingDirection * walkSpeed * Time.deltaTime;
+    if (!Physics2D.Raycast(transform.position, (Vector2.down + Vector2.right),5.0f, 1) && walkingDirection == 1.0f)
+         walkingDirection = -1.0f;
+    else if (!Physics2D.Raycast(transform.position, (Vector2.down + Vector2.left), 5.0f, 1) && walkingDirection == -1.0f)
+         walkingDirection = 1.0f;
+     transform.Translate(walkAmount);
+	}
+}
