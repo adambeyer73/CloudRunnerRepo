dd9af25f0d986471c324f3aeb82a35239fc5634c
joey-borowicz <joseph.borowicz@ndsu.edu>
2016-04-21 21:59:36 -0500

    A whole bunch of assets for level 1

---
+++ Assets/Scripts/BagInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Inventory
+{
+    interface BagInterface
+    {
+         void addItem(PowerUps item);
+         List<Inventory.PowerUps> viewItems();
+
+    }
+}
